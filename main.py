a9df78c377882ccb0b15ed1b9e919f611e8b0dd7
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-12-11 05:27:16 +0000

    1st commit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
-
-# ******************************
-# Make your Code
-# ******************************
-
-# print (num3) 
+num3 = []
+for i in range(len(num1)):
+    num3.append(num1[i])
+    num3.append(num2[i])
+print (num3) 
