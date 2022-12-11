c6ab2689ff2317516255c8b1c4ad638970e53824
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-12-11 05:36:26 +0000

    2nd commit

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 num3 = []
-for i in range(len(num1)):
-    num3.append(num1[i])
-    num3.append(num2[i])
+smaller = len(num2)
+if len(num1) < len(num2):
+    smaller = len(num1)
+
+for i in range(smaller):
+    num3.append(num1[0])
+    num1.pop(0)
+    num3.append(num2[0])
+    num2.pop(0)
+num3.append(num1 + num2)
 print (num3) 
